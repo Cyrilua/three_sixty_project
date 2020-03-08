5846a81db7a6c52598f37dc0975b7fd1d20d5f45
Cyrilua <44938881+Cyrilua@users.noreply.github.com>
2020-03-08 22:14:22 +0500

    Базовые классы #TODO

--- three_sixty/models.py
+++ three_sixty/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
+class User (models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    patronymic = models.CharField(max_length=20)
+    #TODO
+
+
+class EvaluationMessage (models.Model):
+    #TODO
+
+
+class SocialCircle (models.Model):
+    def __init__(self, upper_circe, lower_circle):
+        self.upper_circe = SocialCircle
+        self.lower_circle = SocialCircle
+    #TODO
+
+
+
+
+
 # Create your models here.
