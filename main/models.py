971be1bd605bab53cca7d6baeaa22a8614472cc6
Sw24sX <aleksandr.korolyov.99@users.noreply.github.com>
2020-03-26 18:27:25 +0500

    Добавил первичные модели базы данных

--- main/models.py
+++ main/models.py
@@ -22,6 +22,60 @@ class Profile (models.Model):
         return 'Profile for user {} {}'.format(self.name, self.surname)
 
 
+class Company(models.Model):
+    name = models.CharField(max_length=20)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    workers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=50)
+    workers = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=20)
+    workers = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+
+class Command(models.Model):
+    name = models.CharField(max_length=20)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    workers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+
+
+class UserCommand(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    commands = models.ForeignKey(Command, on_delete=models.CASCADE)
+
+
+class Poll(models.Model):
+    assessed = models.OneToOneField(User, on_delete=models.CASCADE)
+    respondent = models.OneToOneField(User, on_delete=models.CASCADE, related_name='+')
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+class Questions(models.Model):
+    question = models.CharField(max_length=100)
+
+
+class Answers(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=50)
+
+
+class OpenQuestions(models.Model):
+    question = models.CharField(max_length=100)
+
+
+class OpenAnswer(models.Model):
+    open_question = models.OneToOneField(OpenQuestions, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=200)
+
+
 class EvaluationMessage (models.Model):
     def __init__(self):
         return
