6c7afa7d5ba003bb82ef89139a9d4052d75dbcfc
Cyrilua <44938881+Cyrilua@users.noreply.github.com>
2020-04-08 15:40:11 +0500

    Небольшие косметические изменения: - Правильное отображение менюв шапке на всех страницах - Исключил index.html, теперь все красиво переадресуется (потестируйте) - Стили на странице регистрации все еще не работают

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path('user/', views.user_view, name='user'),
-    path('', views.index_view, name='index'),
+    path('', views.user_login, name='login'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('groups/', views.groups_view, name='groups'),
