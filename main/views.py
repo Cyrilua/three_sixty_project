3d25fe3f5abeb1d3602a07899fab66dc0301a779
Alllex202 <Alllex.202@gmail.com>
2020-04-11 15:30:09 +0500

    Вытащил меню в отдельный файл и изменил ссылку .../user/ на .../profile/ .

--- main/views.py
+++ main/views.py
@@ -18,7 +18,7 @@ def user_view(request):
     args = {"title": "Мой профиль"}
     if auth.get_user(request).is_authenticated:
         args['profile'] = get_user_profile(request)
-    return render(request, 'main/user.html', args)
+    return render(request, 'main/profile.html', args)
 
 
 def other_user_view(request, profile_id):
@@ -223,7 +223,7 @@ def get_all_users_in_company(request):
 
 def user_register(request):
     if auth.get_user(request).is_authenticated:
-        return redirect('/user')
+        return redirect('/profile')
     args = {'user_form': UserCreationForm(), 'profile_form': ProfileForm()}
     if request.method == 'POST':
 
@@ -252,7 +252,7 @@ def user_register(request):
 
 def user_login(request):
     if auth.get_user(request).is_authenticated:
-        return redirect('/user')
+        return redirect('/profile')
     args = {'title': "Вход"}
     if request.POST:
         username = request.POST.get("username", '').lower()
@@ -260,7 +260,7 @@ def user_login(request):
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('/user')
+            return redirect('/profile')
         else:
             args['login_error'] = "Неверный логин или пароль"
             args['username'] = username
@@ -337,7 +337,7 @@ def groups_view(request):
         for j in users:
             print('    ' + j.__str__())
     ####
-    return render(request, 'main/communication.html', {
+    return render(request, 'main/communications.html', {
         'title': "Группы",
         'teams': teams,
         'profile': profile,
