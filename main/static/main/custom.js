a7c42624f8c6b5c4a67eee7f785889b26bd39da3
Alllex202 <Alllex.202@gmail.com>
2020-04-30 23:59:38 +0500

    Изменил поля для ввода ответа на опрос + счетчик символов

--- main/static/main/custom.js
+++ main/static/main/custom.js
@@ -1,14 +1,19 @@
 function copyText(el) {
-    var $tmp = $("<input>");
+    let $tmp = $("<input>");
     $("body").append($tmp);
     $tmp.val($(el).text()).select();
     document.execCommand("copy");
     $tmp.remove();
 }
 
-
 function updateTextForRange(el1, el2) {
     let range = document.getElementById(el1);
     let label = document.getElementById(el2);
     label.innerText = range.value;
+}
+
+function getCounterSymbols(el1, el2) {
+    let text = document.getElementById(el1);
+    let counter = document.getElementById(el2);
+    counter.innerText = (500 - text.value.length).toString();
 }
