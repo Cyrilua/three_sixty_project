d13a042a73483059d677ef831a972aaf5eb19b4d
Alllex202 <Alllex.202@gmail.com>
2020-05-12 23:58:30 +0500

    Update filterForRespondentChoice.js

--- main/static/main/filterForRespondentChoice.js
+++ main/static/main/filterForRespondentChoice.js
@@ -1,10 +1,10 @@
-var filter_select_el = document.getElementById('filter-position');
-var items_el = document.getElementById('result');
+let filter_select_el = document.getElementById('filter-position');
+let items_el = document.getElementById('result');
 
 filter_select_el.onchange = function() {
 	console.log(this.value);
-  var items = items_el.getElementsByClassName('position');
-  for (var i=0; i<items.length; i++) {
+  let items = items_el.getElementsByClassName('position');
+  for (let i=0; i<items.length; i++) {
   	if (items[i].classList.contains(this.value)) {
     	items[i].style.display = '';
     } else {
