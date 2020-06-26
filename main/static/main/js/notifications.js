19911944e15927c0ed6aa8619f40405d9d799bfd
Alllex202 <Alllex.202@gmail.com>
2020-06-26 19:43:47 +0500

    Уведомления - скролл вверх

--- main/static/main/js/notifications.js
+++ main/static/main/js/notifications.js
@@ -1,4 +1,24 @@
 $(function () {
+    // Скролл вверх
+    $('.notifications-scroll-top-btn').click(function () {
+        console.log('asd');
+        $('body, html').animate({scrollTop: 0}, 400);
+    });
+    $(window).scroll(function () {
+        let classList = $('.notifications-scroll-top-btn')[0].classList;
+        if ((function () {
+            return $(this).scrollTop() >= $(this).height() * 0.5
+        })()) {
+            if (!classList.contains('notifications-scroll-top-btn-visible')) {
+                classList.add('notifications-scroll-top-btn-visible');
+            }
+        } else {
+            if (classList.contains('notifications-scroll-top-btn-visible')) {
+                classList.remove('notifications-scroll-top-btn-visible');
+            }
+        }
+    });
+
     // Удаление одного уведомления
     $('.notification-button-cross').click(function () {
         let el = $(this)[0].parentElement.parentElement;
