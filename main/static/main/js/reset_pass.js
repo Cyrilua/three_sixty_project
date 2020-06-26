65a3b84f0f105d0c4766fabbe98745591847d2b7
Alllex202 <Alllex.202@gmail.com>
2020-06-26 15:01:56 +0500

    Сброс пароля - обновляется таймер

--- main/static/main/js/reset_pass.js
+++ main/static/main/js/reset_pass.js
@@ -1,4 +1,5 @@
 $(function () {
+    let time;
     let $input = $('.reset-pass-email'),
         $buffer = $('.input-buffer');
 
@@ -6,19 +7,35 @@ $(function () {
         $buffer.text($input.val());
         $input.width($buffer.width());
     });
+    setTimeout(timeOut, 1000, $('span.reset-pass-timer'));
 
-    setTimeout(timeOut, 1000, $('.reset-pass-timer'));
+    function timeOut(timer) {
+        if (typeof(time) == 'undefined') {
+            time = timer.text();
+        }
+        if (timer.text() > 0) {
+            timer.text(timer.text() - 1);
+            setTimeout(timeOut, 1000, timer);
+        } else {
+            let el = $('p.reset-pass-repeat-letter')[0];
+            const parent = el.parentElement;
+            el.remove();
+            el = document.createElement('a');
+            el.href = '#';
+            el.classList.add('reset-pass-repeat-link');
+            el.text = 'Отправить письмо ещё раз';
+            parent.appendChild(el);
+            $('a.reset-pass-repeat-link').click(function () {
+                let el = $(this)[0];
+                const parent = el.parentElement;
+                el.remove();
+                el = document.createElement('p')
+                el.classList.add('reset-pass-repeat-letter');
+                el.innerHTML = 'Отправить письмо ещё раз (доступно через <span class="reset-pass-timer">' + time + '</span> с.)';
+                parent.appendChild(el);
+                setTimeout(timeOut, 1000, $('span.reset-pass-timer'));
+            });
+        }
+    }
 });
 
-function timeOut(timer) {
-    if (timer.text() > 0) {
-        timer.text(timer.text() - 1);
-        setTimeout(timeOut, 1000, timer);
-    } else {
-        $('.reset-pass-repeat-letter')
-            .text("Отправить письмо ещё раз")
-            .prop({
-                'href': '#',
-            });
-    }
-}
