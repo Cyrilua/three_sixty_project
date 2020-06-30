e6b71f9b87502d03e2dfd70fa15baebb0c796092
Alllex202 <Alllex.202@gmail.com>
2020-06-30 19:48:32 +0500

    Создание опросов - начал делать первый этап создания

---
+++ main/static/main/js/new_poll_view.js
@@ -0,0 +1,34 @@
+$(function () {
+    const csrf = $('input[name="csrfmiddlewaretoken"]').val();
+    const search = $('#search');
+    const listItems = $('.new-poll-selection-list').children()
+    const listItemValues = listItems.children('.new-poll-selection-list-item-name').children('.new-poll-selection-list-item-checkbox');
+    const btnNext = $('.new-poll-to-step-2')[0];
+
+    // Переход к следующему этапу
+    btnNext.click(function () {
+        $.ajax({
+            url: '',
+            type: 'post',
+            data: {
+                //TODO
+                csrfmiddlewaretoken: csrf,
+            },
+            success: function (response) {
+                //TODO
+            }
+        });
+    });
+
+    // Поиск
+    search.on('input', function () {
+        console.log();
+        findSubstring(search.val().toLowerCase())
+    });
+
+    function findSubstring(substr) {
+        for (let i = 0; i < listItemValues.length; i++) {
+            listItems[i].hidden = (listItemValues[i].value.toLowerCase()).indexOf(substr) === -1;
+        }
+    }
+});
