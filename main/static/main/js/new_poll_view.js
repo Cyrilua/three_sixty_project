6696c7f0515a44ccb6d87445e9cd7754e27972a5
Alllex202 <Alllex.202@gmail.com>
2020-07-01 16:36:46 +0500

    начал делать создание  опросов

--- main/static/main/js/new_poll_view.js
+++ main/static/main/js/new_poll_view.js
@@ -1,41 +1,27 @@
 $(function () {
-    const csrf = $('input[name="csrfmiddlewaretoken"]').val();
-    let div = $('.new-poll')[0];
-    $('#test').click(function () {
-        $.ajax({
-            url: '',
-            type: 'post',
-            data: {
-                csrfmiddlewaretoken: csrf,
-            },
-            success: function (response) {
-                div.innerHTML = response.newHTML;
-                console.log(response.newHTML);
-            }
-        })
-    })
-
     // const csrf = $('input[name="csrfmiddlewaretoken"]').val();
+    const container = $('.new_poll-container');
+    const rightMenu = $('.new_poll-menu-right');
     // const search = $('#search');
     // const listItems = $('.new-poll-selection-list').children()
     // const listItemValues = listItems.children('.new-poll-selection-list-item-name').children('.new-poll-selection-list-item-checkbox');
     // const btnNext = $('.new-poll-to-step-2')[0];
-    //
-    // // Переход к следующему этапу
-    // btnNext.click(function () {
-    //     $.ajax({
-    //         url: '',
-    //         type: 'post',
-    //         data: {
-    //             //TODO
-    //             csrfmiddlewaretoken: csrf,
-    //         },
-    //         success: function (response) {
-    //             //TODO
-    //         }
-    //     });
-    // });
-    //
+    $('.new_poll-create-btn').click(function () {
+        $.ajax({
+            url: '/poll/editor/',
+            type: 'get',
+            data: {
+
+            },
+            success: function (response) {
+                container[0].innerHTML = response.pageContainer;
+                rightMenu[0].innerHTML = response.rightMenu;
+            }
+        });
+    });
+
+
+
     // // Поиск
     // search.on('input', function () {
     //     console.log();
@@ -47,4 +33,22 @@ $(function () {
     //         listItems[i].hidden = (listItemValues[i].value.toLowerCase()).indexOf(substr) === -1;
     //     }
     // }
+
+    // ################################################################################
+
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            let cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                let cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
 });
