39c568c556671097a5329e15db48f588087f96ea
Sw24sX <56915392+Sw24sX@users.noreply.github.com>
2020-07-06 22:53:35 +0500

    save

--- three_sixty_project/settings.py
+++ three_sixty_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .prod_settings import *
+#from .prod_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#try:
-#    from .local_settingssss import *
-#except ImportError:
-#    from .prod_settings import *
+try:
+    from .local_settingssss import *
+except ImportError:
+    from .prod_settings import *
