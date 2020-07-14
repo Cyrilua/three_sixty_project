e76bb8a42d1f26ac61b8c9299993db3108ad14f3
Alllex202 <Alllex.202@gmail.com>
2020-07-14 16:38:39 +0500

    Меня в шапке

--- main/static/main/js/custom.js
+++ main/static/main/js/custom.js
@@ -18,8 +18,34 @@ function getCounterSymbols(el1, el2) {
     counter.innerText = (500 - text.value.length).toString();
 }
 
-// $(function () {
-//     $('body').on('mouseup', '.btn', function () {
-//         $(this).blur();
-//     })
-// });
+$(function () {
+    let headMenu = $('.head-menu');
+    let menuUp = headMenu.children('.head-menu-up');
+    let menuDown = headMenu.children('.head-menu-down');
+    let body = $('body');
+
+    body.on('click', '.head-menu-up', function () {
+        menuUp.toggleClass('active');
+        menuDown.toggle();
+    });
+
+    body.on('click', '.container-fluid', function () {
+        if (menuDown.css('display') !== 'none') {
+            menuUp.toggleClass('active');
+            menuDown.toggle();
+        }
+    });
+
+    body.on('click', '.head-menu-item', function () {
+        let id = this.id;
+        if (id === 'head-menu-company') {
+            $(location).attr({href: '/company_view/'});
+        } else if (id === 'head-menu-settings') {
+            $(location).attr({href: '/edit/'});
+        } else if (id === 'head-menu-logout') {
+            $(location).attr({href: '/logout/'});
+        } else {
+            throw new Error('invalid argument value');
+        }
+    });
+});
