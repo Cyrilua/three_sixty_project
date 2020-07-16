267fb3b6ddf1954def291cc543de9018801008b1
Sw24sX <56915392+Sw24sX@users.noreply.github.com>
2020-07-16 13:44:03 +0500

    fix

--- main/views/validators.py
+++ main/views/validators.py
@@ -19,7 +19,7 @@ def validate_login(login: str):
         result.append('Введите логин')
     if len(login) < 3:
         result.append('Минимальная длинна логина - 3 символа')
-    if not login[0].isalpha():
+    if len(login) > 0 and not login[0].isalpha():
         result.append('Логин должен начинаться с буквы')
 
     reg = re.compile('[^a-z0-9_]')
