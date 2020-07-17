c30537fda8599fff3c819f4f463dde93ea6f7271
Alllex202 <Alllex.202@gmail.com>
2020-07-17 15:08:03 +0500

    fix profile

--- main/static/main/js/profile.js
+++ main/static/main/js/profile.js
@@ -4,6 +4,17 @@ $(function () {
     let sortable = $('.center-content-notifications-sort');
     let categories = $('.center-content-notifications-categories');
     let categorySubstrate  = $('.notifications-sort-substrate');
+    let name = $('.center-content-information-name')
+    let aCompany = $('.center-content-information-company');
+
+    // Расположеие ссылки на компанию
+    if (parseFloat(name.width()) + parseFloat(name.css('margin-left')) + parseFloat(name.css('margin-right')) + 30 + parseFloat(aCompany.width()) <= 680) {
+        aCompany.css({
+            'position': 'absolute',
+            'right': '0',
+            'bottom': 'calc(100% + 14px)'
+        })
+    }
 
     // Сортировка поднимается над контентом
     $(window).scroll(function () {
