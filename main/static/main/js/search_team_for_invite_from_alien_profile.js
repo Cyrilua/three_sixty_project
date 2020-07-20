aef9053f29bbd41c1421771c2b5810363ee7db50
Alllex202 <Alllex.202@gmail.com>
2020-07-20 19:17:11 +0500

    bugfix and code cleanup

--- main/static/main/js/search_team_for_invite_from_alien_profile.js
+++ main/static/main/js/search_team_for_invite_from_alien_profile.js
@@ -1,34 +1,29 @@
 $(function () {
     let body = $('body');
-    let search = $('.search__input');
+    // let search = $('.search__input');
     let result = $('.result');
     let resultTeams = result.children('.teams');
     let teams = resultTeams.children('.team');
-    let resultEmpty = result.children('.result__empty');
+    // let resultEmpty = result.children('.result__empty');
     let resultNotFind = result.children('.result__not-find');
 
     // Поиск по названию
     body.on('input', '.search__input', function () {
         if (teams.length > 0) {
             let filter = $(this).val().toLowerCase();
-            console.log(filter)
             let countResult = 0;
             for (let i = 0; i < teams.length; i++) {
                 let team = $(teams[i]);
                 let teamName = team.attr('data-name').toLowerCase();
-                console.log(team, teamName)
                 if (teamName.indexOf(filter) !== -1) {
-                    console.log('show')
                     countResult++;
                     team.removeClass('hide');
                     team.addClass('show');
                 } else {
-                    console.log('hide')
                     team.removeClass('show');
                     team.addClass('hide');
                 }
             }
-            console.log(countResult)
             if (countResult === 0) {
                 if (resultTeams.hasClass('show')) {
                     resultTeams.removeClass('show');
