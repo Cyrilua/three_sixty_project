5db314f103ff63f495dfb3a1cd3beb6cca2de9a3
Alllex202 <Alllex.202@gmail.com>
2020-08-04 17:27:12 +0500

    upd

--- main/static/main/js/new_poll_editor.js
+++ main/static/main/js/new_poll_editor.js
@@ -5,6 +5,8 @@ const timeAnimation = 200;
 $(function () {
     const body = $('body');
 
+    // $('.category-content').removeClass('hide');
+
     body.on('click', '.color__variable', function () {
         $('.color__variable').removeClass('color__variable--select');
         $(this).addClass('color__variable--select');
