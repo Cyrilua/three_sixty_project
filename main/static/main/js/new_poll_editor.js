8635560df71de5a7fe1fd4a84f1725259eaaa84c
Alllex202 <Alllex.202@gmail.com>
2020-08-05 18:35:39 +0500

    upd Новый Редактор опросов

--- main/static/main/js/new_poll_editor.js
+++ main/static/main/js/new_poll_editor.js
@@ -6,15 +6,12 @@ $(function () {
     const body = $('body');
     const pollHeader = $('.poll-editor__header');
 
+    // let questions = {};
+
+
     // $('.category-content').removeClass('hide');
 
-    // Сортировка
-    if ($('.mdc-select').length > 0) {
-        const sortable = new mdc.select.MDCSelect(document.querySelector('.mdc-select'));
-        sortable.listen('MDCSelect:change', () => {
-                console.log(sortable.value)
-        });
-    }
+    run();
 
     // Смена цвета у опроса
     body.on('click', '.color__variable', function () {
@@ -23,7 +20,18 @@ $(function () {
         let color = $(this).attr('data-color');
         pollHeader.removeClass('red blue purple');
         if (color !== '') {
-            pollHeader.addClass(color)
+            pollHeader.addClass(color);
+            if (color === 'red') {
+                document.documentElement.style.setProperty('--mdc-theme-primary', '#FF1841');
+            } else if (color === 'blue') {
+                document.documentElement.style.setProperty('--mdc-theme-primary', '#001AFF');
+            } else if (color === 'purple') {
+                document.documentElement.style.setProperty('--mdc-theme-primary', '#DB00FF');
+            } else {
+                throw new Error('Unexpected attribute on color change');
+            }
+        } else {
+            document.documentElement.style.setProperty('--mdc-theme-primary', '#C4C4C4');
         }
     });
 
@@ -32,9 +40,27 @@ $(function () {
         countLines(this, 5);
     });
 
+    body.on('input', '.textarea-border', function () {
+        countLines(this, -1);
+    });
+
     // Автоувеличение полей ввода
     function countLines(el, delta) {
         el.style.height = '1px';
         el.style.height = (el.scrollHeight + delta) + 'px';
+    }
+
+    function run() {
+        // Инициализация сортировки
+        $('.question').each(function (key, el) {
+            // let numberQuestion = el.getAttribute('data-question');
+            let sortable = new mdc.select.MDCSelect(el.querySelector('.mdc-select'));
+            sortable.listen('MDCSelect:change', () => {
+                console.log(sortable.value)
+            });
+            // questions[`question_${numberQuestion}`] = {
+            //     'sortable': sortable,
+            // };
+        });
     }
 });
