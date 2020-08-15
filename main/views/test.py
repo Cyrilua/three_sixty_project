a2bb65d4198b36d27ee94866b058f74795735be8
Sw24sX <56915392+Sw24sX@users.noreply.github.com>
2020-08-15 16:56:06 +0500

    save

--- main/views/test.py
+++ main/views/test.py
@@ -2,15 +2,12 @@ from django.shortcuts import redirect
 from django.shortcuts import render
 from . import notifications_views
 from .auxiliary_general_methods import *
-from main.models import TestTable, Profile, NeedPassPoll
+from main.models import TestTable, Profile, NeedPassPoll, Poll
 from django.http import JsonResponse
 
 
 def test(request):
-    profile = Profile.objects.get(id=71)
-    res = NeedPassPoll.objects.filter(profile=profile).values('profile').values('user')
-    print(res)
-    for need_pass_poll in res:
-        print(need_pass_poll)
-
+    poll = Poll.objects.get(id=61)
+    result = [i.profile.user.email for i in NeedPassPoll.objects.filter(poll=poll)]
+    print(result)
     return render(request, 'main/test.html')
