086f024e90992efd784522fedf81ea60a811ad00
Alllex202 <Alllex.202@gmail.com>
2020-08-24 14:35:20 +0500

    Изменения ссылок в профилях

--- main/static/main/js/custom.js
+++ main/static/main/js/custom.js
@@ -1,22 +1,22 @@
-function copyText(el) {
-    let $tmp = $("<input>");
-    $("body").append($tmp);
-    $tmp.val($(el).text()).select();
-    document.execCommand("copy");
-    $tmp.remove();
-}
-
-function updateTextForRange(el1, el2) {
-    let range = document.getElementById(el1);
-    let label = document.getElementById(el2);
-    label.innerText = range.value;
-}
-
-function getCounterSymbols(el1, el2) {
-    let text = document.getElementById(el1);
-    let counter = document.getElementById(el2);
-    counter.innerText = (500 - text.value.length).toString();
-}
+// function copyText(el) {
+//     let $tmp = $("<input>");
+//     $("body").append($tmp);
+//     $tmp.val($(el).text()).select();
+//     document.execCommand("copy");
+//     $tmp.remove();
+// }
+//
+// function updateTextForRange(el1, el2) {
+//     let range = document.getElementById(el1);
+//     let label = document.getElementById(el2);
+//     label.innerText = range.value;
+// }
+//
+// function getCounterSymbols(el1, el2) {
+//     let text = document.getElementById(el1);
+//     let counter = document.getElementById(el2);
+//     counter.innerText = (500 - text.value.length).toString();
+// }
 
 $(function () {
     console.log('Time loading page ' + performance.now());
