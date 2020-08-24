8f1a504bf54166656287facf58d6d11bb590cbdc
Sw24sX <56915392+Sw24sX@users.noreply.github.com>
2020-08-24 16:09:58 +0500

    delete old and not used views

--- main/urls.py
+++ main/urls.py
@@ -12,102 +12,67 @@ from .urls_loadable import register_urls, edit_profile_urls, polls_view_urls, po
 
 app_name = "main"
 urlpatterns = [
-                  # Регистрация
-                  path('register/', include(register_urls)),
-                  # Начальная страница
-                  path('', user_views.user_login, name='login'),
-                  # Выход
-                  path('logout/', user_views.user_logout, name='logout'),
+    # Регистрация
+    path('register/', include(register_urls)),
+    # Начальная страница
+    path('', user_views.user_login, name='login'),
+    # Выход
+    path('logout/', user_views.user_logout, name='logout'),
 
-                  # Сообщение об успешной смене пароля
-                  path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
-                      template_name='main/password/password_change_done.html'),
-                       name='password_change_done'),
-                  # Изменение пароля
-                  path('password_change/',
-                       auth_views.PasswordChangeView.as_view(template_name='main/password/password_change.html',
-                                                             success_url=reverse_lazy('main:password_change_done')),
-                       name='password_change'),
-                  # Сообщение об отправке сообщения на почту
-                  path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(
-                      template_name='main/password/password_reset_done.html'),
-                       name='password_reset_done'),
-                  # Неведомая и странно работающая часть
-                  path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-                      template_name='main/password/password_reset_confirm.html',
-                      success_url=reverse_lazy('main:login')
-                  ),
-                       name='password_reset_confirm'),
-                  # Сброс пароля
-                  path('password_reset/', auth_views.PasswordResetView.as_view(
-                      template_name='main/password/password_reset_form.html',
-                      subject_template_name='main/password/password_reset_subject.txt',
-                      email_template_name='main/password/password_reset_email.html',
-                      success_url=reverse_lazy('main:password_reset_done')),
-                       name='password_reset'),
-                  # Сообщение об успешном сбросе пароля
-                  path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-                      template_name='main/password/old/password_reset_complete.html'),
-                       name='password_reset_complete'),
+    # Сообщение об успешной смене пароля
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
+      template_name='main/password/password_change_done.html'),
+       name='password_change_done'),
+    # Изменение пароля
+    path('password_change/',
+       auth_views.PasswordChangeView.as_view(template_name='main/password/password_change.html',
+                                             success_url=reverse_lazy('main:password_change_done')),
+       name='password_change'),
+    # Сообщение об отправке сообщения на почту
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(
+      template_name='main/password/password_reset_done.html'),
+       name='password_reset_done'),
+    # Неведомая и странно работающая часть
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+      template_name='main/password/password_reset_confirm.html',
+      success_url=reverse_lazy('main:login')
+    ),
+       name='password_reset_confirm'),
+    # Сброс пароля
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+      template_name='main/password/password_reset_form.html',
+      subject_template_name='main/password/password_reset_subject.txt',
+      email_template_name='main/password/password_reset_email.html',
+      success_url=reverse_lazy('main:password_reset_done')),
+       name='password_reset'),
+    # Сообщение об успешном сбросе пароля
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+      template_name='main/password/old/password_reset_complete.html'),
+       name='password_reset_complete'),
 
-                  # Просмотр профиля
-                  path('<int:profile_id>/', render_profile.profile_view, name='profile'),
-                  # Редактирование профиля
-                  path('edit/', include(edit_profile_urls), name='edit'),
+    # Просмотр профиля
+    path('<int:profile_id>/', render_profile.profile_view, name='profile'),
+    # Редактирование профиля
+    path('edit/', include(edit_profile_urls), name='edit'),
 
-############################ Rework #################### (start)
-                  # Промотр конкретной команды
-                  path('team/<int:group_id>/', teams_views.team_user_view, name='team_view'),
-                  # Создание команды
-                  path('create_team/', teams_views.create_team, name='create_team'),
-                  # Присоединение к команде (по ключу)
-                  path('connect_to_command/', teams_views.connect_to_team_to_key, name='connect_to_group'),
-                  # Присоединение к команде (по ссылке)
-                  path('invite/t/<str:key>/', teams_views.connect_to_team_to_link, name='connect_to_team_to_link'),
-                  # Поиск команды для присоединения в нее
-                  path('<int:profile_id>/invite/', teams_views.search_team_for_invite, name='search_team_for_invite'),
-                  # Отправить уведомление о приглашении
-                  path('<int:profile_id>/invite/<int:team_id>/', teams_views.send_notification_profile),
+    # Промотр конкретной команды
+    path('team/<int:group_id>/', teams_views.team_user_view, name='team_view'),
+    # Поиск команды для присоединения в нее
+    path('<int:profile_id>/invite/', teams_views.search_team_for_invite, name='search_team_for_invite'),
 
 
-                  # Создание компании (для ясности стоит изменить url)
-                  path('add_company/', company_views.create_company, name='add_company'),
-                  # Присоединение к компании (по ключу)
-                  path('connect_to_company/', company_views.connect_to_company_to_key, name='connect_to_company'),
-                  # Присоеддинение к компании (по ссылке)
-                  path('invite/c/<str:key>/', company_views.connect_to_company_to_link,
-                       name="connect_to_company_to_link"),
-                  # Возвращает список пользователей в компани
-                  path('get_all_companies_users/', company_views.get_all_users_in_company,
-                       name='get_all_companies_users'),
-                  # Добавление новой платформы (может сделать человек, не состоящий в компании)
-                  path('add_new_platform/', company_views.add_new_platform, name='add_new_platform'),
-                  # Добавление новой должности (может сделать человек, не состоящий в компании)
-                  path('add_new_position/', company_views.add_new_position, name='add_new_position'),
-                  # Добавление новой должности в компанию (если должности не существует - она будет создана)
-                  path('add_new_position_in_company/', company_views.add_position_in_company,
-                       name="add_new_position_in_company"),
-                  # Добавление новой платформы в компанию (если платформы не существует - она будет создана)
-                  path('add_new_platform_in_company/', company_views.add_platform_in_company,
-                       name="add_new_platform_in_company"),
-                  # Сохраняет конкретную должность для пользователя
-                  path('choose_position/', company_views.choose_position,
-                       name='position_choice'),
-                  # Сохраняет конкретную платформу для пользователя
-                  path('choose_platform/', company_views.choose_platform,
-                       name="platform_choice"),
-############################ Rework #################### (end)
+    # Страница просмотра опросов и шаблонов
+    path('polls/', include(polls_view_urls), name='new_poll_view'),
+    # Создание нового
+    path('poll/', include(poll_urls), name='poll'),
+    # Страница компании
+    path('company/<int:id_company>/', include(company_url), name='company_view'),
+    # Страница команд
+    path('teams/', include(teams_url), name='teams_view'),
 
-                  # Страница просмотра опросов и шаблонов
-                  path('polls/', include(polls_view_urls), name='new_poll_view'),
-                  # Создание нового
-                  path('poll/', include(poll_urls), name='poll'),
-                  # Страница компании
-                  path('company/<int:id_company>/', include(company_url), name='company_view'),
-                  # Страница команд
-                  path('teams/', include(teams_url), name='teams_view'),
+    ############ Only for debug ###############
+    path('test/', test.test),
+    # Создание компании (для ясности стоит изменить url)
+    path('add_company/', company_views.create_company, name='add_company'),
 
-                  ############ Only for debug ###############
-                  path('test/', test.test)
-
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views/teams_views.py
+++ main/views/teams_views.py
@@ -9,98 +9,9 @@ from django.shortcuts import redirect, render
 from django.http import JsonResponse
 
 
-def create_team(request) -> render:
-    if auth.get_user(request).is_anonymous:
-        return redirect('/')
-
-    args = {
-        'title': 'Создание новой команды',
-        'team_form': TeamForm()
-    }
-
-    user = auth.get_user(request)
-    profile = Profile.objects.get(user=user)
-
-    if request.method == "POST":
-        team_form = TeamForm(request.POST)
-
-        if team_form.is_valid():
-            new_team = team_form.save(commit=False)
-            new_team.owner = user
-            new_team.key = uuid.uuid4().__str__()
-            new_team.save()
-
-            profile.groups.add(new_team)
-            profile.groups.add()
-        return redirect('/')
-    return render(request, 'main/teams/old/add_new_team.html', args)
-
-
-def connect_to_team_to_key(request) -> render:
-    if auth.get_user(request).is_anonymous:
-        return redirect('/')
-
-    args = {'title': 'Присоединиться к комманде'}
-    profile = get_user_profile(request)
-
-    if request.method == "POST":
-        try:
-            key_group = request.POST.get('key', '')
-            group = Group.objects.get(key=key_group)
-            if group in profile.groups.all():
-                args['error'] = "Пользователь уже состоит в этой команде"
-                return render(request, 'main/teams/old/connect_to_team.html', args)
-        except:
-            args['error'] = "Ключ не существует или введен неверно"
-            return render(request, 'main/teams/old/connect_to_team.html', args)
-        else:
-            profile.groups.add(group)
-            profile.save()
-            return redirect('/')
-    return render(request, 'main/teams/old/connect_to_team.html', args)
-
-
-def connect_to_team_to_link(request, key: str) -> render:
-    if auth.get_user(request).is_anonymous:
-        return redirect('/')
-
-    profile = get_user_profile(request)
-
-    try:
-        group = Group.objects.get(key=key)
-        if group in profile.groups.all():
-            return redirect("/team/{}/".format(group.id))
-    except:
-        return redirect('/')
-    else:
-        profile.groups.add(group)
-        profile.save()
-        return redirect("/team/{}/".format(group.id))
-
-
 def team_user_view(request, group_id: int) -> render:
     args = {}
 
-    # if auth.get_user(request).is_anonymous:
-    #     return redirect('/')
-    # try:
-    #     group = Group.objects.get(id=group_id)
-    #     profile = get_user_profile(request)
-    #     if profile not in group.profile_set.all():
-    #         raise Exception()
-    # except:
-    #     return redirect('/')
-    #
-    # args['users'] = group.profile_set.all()
-    #
-    # if auth.get_user(request).id == group.owner.id:
-    #     args['link_to_enter'] = request.scheme + "://" + request.get_host() + "/invite/t/" + group.key
-    # args['owner'] = {'pk': group.owner.profile.pk,
-    #                  'name': group.owner.profile.name,
-    #                  'surname': group.owner.profile.surname}
-    # args['team_name'] = group.name
-    # args['title'] = group.name
-    # args['group_id'] = group_id
     return render(request, 'main/teams/team_view.html', args)
 
 
@@ -116,8 +27,8 @@ def search_team_for_invite(request, profile_id: int) -> render:
     user = auth.get_user(request)
     profile = get_user_profile(request)
     commands = filter(lambda x: x not in alien_commands, profile.groups.all())
-    if not profile_is_owner(request):
-        commands = filter(lambda x: x.owner.id == user.id, commands)
+    #if not profile_is_owner(request):
+    #    commands = filter(lambda x: x.owner.id == user.id, commands)
     args = {
         'title': "Пригласить в команду",
         'teams': build_teams(commands, profile_id)
@@ -138,24 +49,6 @@ def build_teams(commands: filter, alien_profile_id: int) -> list:
         }
         result.append(collected_team)
     return result
-
-
-def send_notification_profile(request, profile_id: int, team_id: int) -> JsonResponse:
-    try:
-        alien_profile = Profile.objects.get(id=profile_id)
-        command = Group.objects.get(id=team_id)
-    except:
-        return JsonResponse({}, status=400)
-
-    profile = get_user_profile(request)
-
-    notifications_views.create_notifications(profile=alien_profile,
-                                             name=command.name,
-                                             type_notification='invite_command',
-                                             key=command.key,
-                                             from_profile=profile)
-
-    return JsonResponse({}, status=200)
 
 
 def teams_view(request):
