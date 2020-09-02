094a897b5bd79f77ae661582033767b0aab595b7
Sw24sX <56915392+Sw24sX@users.noreply.github.com>
2020-09-02 21:21:24 +0500

    save

--- main/views/company_views.py
+++ main/views/company_views.py
@@ -528,11 +528,11 @@ def kick_profile_from_company(request: WSGIRequest, id_company: int, profile_id:
         if changed_profile is None:
             return JsonResponse({}, status=404)
 
-        teams = Group.objects.filter(company=company, profile=changed_profile)
-        for team in teams:
-            team.profile_set.remove(changed_profile)
         changed_profile.platforms.clear()
         changed_profile.positions.clear()
         company.profile_set.remove(changed_profile)
+        teams = Group.objects.filter(company=company, profile=changed_profile)
+        for team in teams:
+            team.profile_set.remove(changed_profile)
 
         return JsonResponse({}, status=200)
